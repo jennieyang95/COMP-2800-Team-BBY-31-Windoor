663900e9d514bf4a2b7d2ecfed2af1275b7bb9b1
JungJae Lee <bcitstudent18@gmail.com>
2020-05-08 02:28:18 -0700

    interactively assign video and description from database

---
+++ Windoor/public/js/javasc.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCw7d1WGvyygchOfHN731FqQfwHUBLtidM",
+    authDomain: "projects-2-88388.firebaseapp.com",
+    databaseURL: "https://projects-2-88388.firebaseio.com",
+    projectId: "projects-2-88388",
+    storageBucket: "projects-2-88388.appspot.com",
+    messagingSenderId: "1055517014926",
+    appId: "1:1055517014926:web:0d36594b9cc92cd51f02df"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+
