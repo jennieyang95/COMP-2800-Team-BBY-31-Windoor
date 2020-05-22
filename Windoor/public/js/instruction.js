41db80c25c98516432d56b55473e05375346980c
JungJae Lee <bcitstudent18@gmail.com>
2020-05-22 14:45:38 -0700

    Stretchkids section fixed, agezone fixed
    agezone, kids "lets explore" didn't work before.

---
+++ Windoor/public/js/instruction.js
@@ -0,0 +1,77 @@
+var videndes = {};
+
+       function getuserchoice() {
+            db.collection("useractselc").doc("catenact").onSnapshot(
+                function (snap) {
+                   
+                   let usercate = snap.data().category;
+                   let useracti = snap.data().activities;
+                  
+                pullvidndes(usercate, useracti)
+                
+
+               })}
+
+       getuserchoice();
+
+      
+       function pullvidndes(a,b) {
+
+        let activideo = b + "v";
+        let actidescrp = b + "d";
+
+            db.collection("instruction").doc(a).onSnapshot(
+                function (snap) {
+              
+                   let activid = snap.data()[activideo];
+                   let actidescr = snap.data()[actidescrp];
+                  
+                   document.getElementById("video").src = activid;
+                   document.getElementById("description").innerHTML = actidescr;
+
+               })}
+
+       getuserchoice();
+
+
+
+       firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    document.getElementById("name").innerHTML = "Enjoy " + user.displayName + "!"
+    document.getElementById("useremail").value = user.email  
+   
+  } else {
+      // No user is signed in.
+  }
+});
+
+    
+
+       function favunvaf() {
+
+
+        
+       
+        document.getElementById("favorite").style.color = "Yellow";
+        let email = document.getElementById("useremail").value;
+        
+
+        db.collection("useractselc").doc("catenact").onSnapshot(
+                function (snap) {
+                   
+                   let usercate = snap.data().category;
+                   let useracti = snap.data().activities;
+
+        db.collection("useraccount").doc(email).collection("favorite").doc(useracti).set({
+  
+          category: usercate,
+          activity: useracti
+         
+  
+
+        })
+
+        })
+      
+
+       }
